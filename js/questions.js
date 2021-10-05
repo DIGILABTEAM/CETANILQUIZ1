e47a59da283cb850d20acaf36171cf88cb207f30
Vikas Gavandi <30316401+DIGILABTEAM@users.noreply.github.com>
2021-10-05 13:05:28 +0530

    deploy Quiz

---
+++ js/questions.js
@@ -0,0 +1,43 @@
+/*All questions in the quiz goes here in the data object.....*/
+var data =
+	[
+		[
+			{
+				question: "Cilnidipine",
+				option1: "Reduces PVR via inhibition of L-/N-type calcium channel in the arteries.",
+				option2: "RAAS suppression.",
+				option3: "Decrease  CO-decrease heart rate & force of contraction.",
+				answer: 1
+			},
+			{
+				question: "Telmisartan",
+				option1: "Reduces PVR via inhibition of L-/N-type calcium channel in the arteries.",
+				option2: "RAAS suppression.",
+				option3: "Decrease  CO-decrease heart rate & force of contraction.",
+				answer: 2
+			},
+			{
+				question: "Metoprolol",
+				option1: "RAAS suppression.",
+				option2: "Decrease CO-decrease heart rate & force of contraction.",
+				option3: "Reduces PVR via inhibition of L-/N-type calcium channel in the arteries.",
+				answer: 2
+			},
+			// {
+			// 	question: "What are three potential causes of a productive cough?",
+			// 	option1: "Bronchitis",
+			// 	option2: "Bacterial infection",
+			// 	option3: "Anaerobic bacterial infection",
+			// 	option4: "All of the above",
+			// 	answer: 4
+			// },
+			// {
+			// 	question: "Dry, centrally painful, non-productive cough points to which of the following condition",
+			// 	option1: "Pneumonia",
+			// 	option2: "tracheitis",
+			// 	option3: "both A and B",
+			// 	option4: "None of the above",
+			// 	answer: 3
+			// }
+		]
+	];
